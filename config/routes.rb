4cb1d962e84d1d86dffa692b97c9634ee2e3d471
Guillaume Balas <guillaume.balas@gmail.com>
2018-10-24 14:46:22 -0500

    Yelp MVP completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'restaurants#index'
+
+  # get 'restaurants', to: 'restaurantsr#index'
+
+  # get 'restaurants/new', to: 'restaurantsr#new', as: 'new_restaurant'
+  # post 'restaurants', to: 'restaurantsr#create'
+
+  # get 'resataurants/:id', to: 'restaurants#show'
+
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: 'new_review'
+  # post 'restaurnts/:id/reviews', to: 'reviews#create'
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :show, :new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 20 fake restaurants...'
+20.times do
+  restaurant = Restaurant.new(
+    name:   Faker::StarWars.planet,
+    address: "#{Faker::Address.full_address}, #{Faker::Address.country}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian'].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
